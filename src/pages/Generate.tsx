5eeb963d4e02492d29a8a958d2be1257c5a02335
Thiago Graciano <msgraciano28@gmail.com>
2026-08-03 20:16:56 -0300

    Feat: Implementa Preview de Thumbnail

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+import { useEffect, useState } from "react";
+import { colorSchemes, dummyThumbnails, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
 import ColorSchemeSelector from "../components/ColorSchemeSelector";
+import PreviewPanel from "../components/PreviewPanel";
 
 const Generate = () => {
 
@@ -20,6 +21,29 @@ const Generate = () => {
     const [style, setStyle] = useState<ThumbnailStyle>("Bold & Graphic");
 
     const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
+
+    const handleGenerate = async () => {
+
+    }
+
+    const fetchThumbnail = async () => {
+        if(id) {
+            const thumbnail : any = dummyThumbnails.find((thumbnail) => thumbnail._id === id);
+            setThumbnail(thumbnail);
+            setAdditionalDetails(thumbnail.user_prompt);
+            setTitle(thumbnail.title);
+            setColorSchemeId(thumbnail.color_scheme);
+            setAspectRatio(thumbnail.aspect_ratio);
+            setStyle(thumbnail.style);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(id) {
+            fetchThumbnail();
+        }
+    },[id])
 
     return (
         <>
@@ -63,7 +87,7 @@ const Generate = () => {
 
                                 {/* Button */}
                                 {!id && (
-                                    <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disable:cursor-not-allowed transition-colors">
+                                    <button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disable:cursor-not-allowed transition-colors">
                                         {loading ? 'Generating...' : 'Generate Thumbnail'}
                                     </button>
                                 )}
@@ -72,7 +96,10 @@ const Generate = () => {
                         </div>
                         {/* Right Panel */}
                         <div>
-
+                            <div className="p-6 rounded-2xl bg-white/8 border border-white/10 shadow-x1">
+                                <h2 className="text-lg font-semibold text-zinc-100 mb-4">Preview</h2>
+                                <PreviewPanel thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio} />
+                            </div>
                         </div>
                     </div>
                 </main>
