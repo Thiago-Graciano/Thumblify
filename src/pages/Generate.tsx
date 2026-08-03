a31c9ff4dfee5b12162ee65fd91ab3be08b761f3
Thiago Graciano <msgraciano28@gmail.com>
2026-08-03 16:15:48 -0300

    Feat: Adiciona Opções de Resolução para criação de Thumb (16:9, 1:1 e 9:16)

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
-import type { IThumbnail } from "../assets/assets";
+import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
+import AspectRatioSelector from "../components/AspectRatioSelector";
 
 const Generate = () => {
 
@@ -11,6 +12,12 @@ const Generate = () => {
 
     const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
     const [loading, setLoading] = useState(false);
+
+    const [aspectRatio, setAspectRatio] = useState<AspectRatio>("16:9");
+    const [colorSchemeId, setColorSchemeId] = useState<string>(colorSchemes[0].id);
+    const [style, setStyle] = useState<ThumbnailStyle>("Bold & Graphic");
+
+    const [styleDropdownOpen, setStyleDropdownOpen] = useState(false);
 
     return (
         <>
@@ -34,7 +41,10 @@ const Generate = () => {
                                             <span className="text-xs text-zinc-400" >{title.length}/100</span>
                                         </div>
                                     </div>
-                                    {/* AspactRatioSelector */}
+                                    {/* AspectRatioSelector */}
+                                    <AspectRatioSelector value={aspectRatio} onChange={setAspectRatio} />
+
+                                    
                                     {/* StyleSelector */}
                                     {/* ColorSchemeSelector */}
 
