e38493932a65f2d48442f482aebf47eabd97782f
Thiago Graciano <msgraciano28@gmail.com>
2026-08-05 00:23:17 -0300

    Feat: Adiciona botões de delete, download e preview

--- src/pages/MyGenerations.tsx
+++ src/pages/MyGenerations.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import SoftBackdrop from "../components/SoftBackdrop"
 import { dummyThumbnails, type IThumbnail } from "../assets/assets"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
+import { ArrowUpRightIcon, DownloadIcon, TrashIcon } from "lucide-react"
 
 const MyGenerations = () => {
 
@@ -92,6 +93,28 @@ const MyGenerations = () => {
                       <span className="px-2 py-0.5 rounded bg-white/8">{thumb.aspect_ratio}</span>
                     </div>
                     <p className="text-xs text-zinc-500" >{new Date(thumb.createdAt!).toDateString()}</p>
+                  </div>
+
+                  <div onClick={(e) => e.stopPropagation()} className="absolute bottom-2 right-2 max-sm:flex sm:hidden group-hover:flex gap-1.5">
+
+                    <TrashIcon
+                      onClick={() => handleDelete(thumb._id)}
+                      className="size-6 bg-black/50 p-1 rounded hover:bg-pink-600 transition-all"
+                    />
+
+                    <DownloadIcon
+                      onClick={() => handleDownload(thumb.image_url!)}
+                      className="size-6 bg-black/50 p-1 rounded hover:bg-pink-600 transition-all"
+                    />
+                    <Link
+                      target="_blank"
+                       to={`/preview?thumbnail_url=${thumb.image_url}&title=${thumb.title}`}
+                    >
+                      <ArrowUpRightIcon
+                        className="size-6 bg-black/50 p-1 rounded hover:bg-pink-600 transition-all"
+                      />
+                    </Link>
+
                   </div>
                 </div>
               )
