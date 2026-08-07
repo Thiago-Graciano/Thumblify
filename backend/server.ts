ac0982e61fbef9d25bf1b5570f1e35ff4663907b
Thiago Graciano <msgraciano28@gmail.com>
2026-08-07 11:41:00 -0300

    Feat: Adiciona Rotas de miniaturas e Controller de usuario

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import AuthRouter from "./routes/AuthRoutes.js";
+import ThumbnailRouter from "./routes/ThumbnailRoutes.js";
 
 declare module "express-session" {
     interface SessionData {
@@ -40,6 +41,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 app.use("/api/auth", AuthRouter);
+app.use("/api/thumbnail", ThumbnailRouter)
 
 const port = process.env.PORT || 3000;
 
