6d75acb7c26c25177249186131ecfd2f569b2760
Thiago Graciano <msgraciano28@gmail.com>
2026-08-07 23:04:28 -0300

    Feat: Adiciona rotas protegidas em generateThumbnail e deleteThumbnail

--- backend/routes/UserRoutes.ts
+++ backend/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUserThumbnails, getThumbnailById } from "../controllers/UserController.js";
+import protect from "../middlewares/auth.js";
 
 const UserRouter = express.Router();
 
-UserRouter.get("/thumbnails", getUserThumbnails);
-UserRouter.get("/thumbnails/:id", getThumbnailById);
+UserRouter.get("/thumbnails", protect, getUserThumbnails);
+UserRouter.get("/thumbnails/:id", protect, getThumbnailById);
 
 export default UserRouter;
