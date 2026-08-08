d83094e8daa4750977a85d7881b5d8c94ae4c935
Thiago Graciano <msgraciano28@gmail.com>
2026-08-08 14:40:14 -0300

    Feat: Integra pagina myGenerations

--- frontend/src/pages/Generate.tsx
+++ frontend/src/pages/Generate.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { colorSchemes, dummyThumbnails, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
+import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from "../assets/assets";
 import SoftBackdrop from "../components/SoftBackdrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
